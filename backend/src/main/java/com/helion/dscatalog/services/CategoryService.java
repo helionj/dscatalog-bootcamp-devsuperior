c90197142c41b0d057b7e8e18e0938af9e922481
helionporto <helionporto@hotmail.com>
2021-05-13 11:19:15 -0400

    Find Category by ID and exception handler

--- backend/src/main/java/com/helion/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/helion/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.helion.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.helion.dscatalog.dto.CategoryDTO;
 import com.helion.dscatalog.entities.Category;
 import com.helion.dscatalog.repositories.CategoryRepository;
+import com.helion.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -23,5 +25,14 @@ public class CategoryService {
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		
 	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id){
+		Optional<Category> obj  = repo.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Objeto não encontrado"));
+		return new CategoryDTO(entity);
+	}
+	
+	
 
 }
