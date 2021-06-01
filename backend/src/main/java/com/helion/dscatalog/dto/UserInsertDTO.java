745b52b7380c146785da684c0f57ca84983576d7
helionporto <helionporto@hotmail.com>
2021-06-01 11:33:07 -0400

    Custom ConstraintValidator

--- backend/src/main/java/com/helion/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/helion/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.helion.dscatalog.dto;
 
+import com.helion.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 
 	private static final long serialVersionUID = 1L;
