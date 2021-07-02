388d75605fa39e569c8c8516b1fa6b681d8e9d9e
helionporto <helionporto@hotmail.com>
2021-07-01 21:09:23 -0400

    Home styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,12 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <h1>Hello DsCatalog</h1>
+      <Home/>
     </>
   );
 }
