061c8342587ab82d2f064a5d0471e91f8018c56c
helionporto <helionporto@hotmail.com>
2021-07-14 14:42:03 -0400

    Pagination, styling

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,50 +1,60 @@
 import { Link } from 'react-router-dom';
+import Pagination from '../../components/Pagination';
 import ProductCard from '../../components/ProductCard';
 import { Product } from '../../types/product';
+import './styles.css';
+
 const product: Product = {
-  "id": 3,
-  "name": "Macbook Pro",
-  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  "price": 1250.0,
-  "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg",
-  "date": "2020-07-14T10:00:00Z",
-  "categories": [
-      {
-          "id": 3,
-          "name": "Computadores"
-      }
-  ]
-}
+  id: 3,
+  name: 'Macbook Pro',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  price: 1250.0,
+  imgUrl:
+    'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg',
+  date: '2020-07-14T10:00:00Z',
+  categories: [
+    {
+      id: 3,
+      name: 'Computadores',
+    },
+  ],
+};
 const Catalog = () => {
   return (
-    <div className="container my-4">
+    <div className="container my-4 catalog-container">
+      <div className="row catalog-title-container">
+        <h1>Catálogo de produtos</h1>
+      </div>
       <div className="row">
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to = "/products/1">
-            <ProductCard product ={ product} />
-          </Link>
-         
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to = "/products/1">
-            <ProductCard product ={ product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
           </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-         <Link to = "/products/1">
-            <ProductCard product ={ product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
           </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to = "/products/1">
-            <ProductCard product ={ product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
           </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to = "/products/1">
-            <ProductCard product ={ product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
           </Link>
         </div>
+        <div className="col-sm-6 col-lg-4 col-xl-3">
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
+        </div>
+      </div>
+      <div className="row">
+        <Pagination/>
       </div>
     </div>
   );
--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,39 +1,46 @@
 import { Link } from 'react-router-dom';
-import {ReactComponent as ArrowIcon} from '../../assets/images/arrowIcon.svg';
+import { ReactComponent as ArrowIcon } from '../../assets/images/arrowIcon.svg';
 import ProductPrice from '../../components/ProductPrice';
-import './styles.css'
+import './styles.css';
 const ProductDetails = () => {
-    return(
-        <div className="product-details-container">
-            <div className="base-card product-details-card">
-                <Link to ="/products">
-                    <div className="goback-container">
-                        <ArrowIcon/>
-                        <h2>VOLTAR</h2>
-                    </div>
-                </Link>
-                
-                <div className="row">
-                    <div className= "col-xl-6">
-                        <div className= "img-container">
-                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg" alt="Nome do produto" />
-                        </div>
-                        <div className="name-price-container">
-                            <h1>Nome do produto</h1>
-                            <ProductPrice price={2345.67}/>
-                        </div>
-                    </div>
-                    <div className= "col-xl-6">
-                        <div className = "description-container">
-                            <h2>Descrição do produto</h2>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis, numquam eum blanditiis rem cumque suscipit. Delectus, sunt explicabo beatae saepe expedita impedit, minima doloribus repellendus repellat quisquam ex atque voluptates?</p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="product-details-container">
+      <div className="base-card product-details-card">
+        <Link to="/products">
+          <div className="goback-container">
+            <ArrowIcon />
+            <h2>VOLTAR</h2>
+          </div>
+        </Link>
 
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg"
+                alt="Nome do produto"
+              />
             </div>
+            <div className="name-price-container">
+              <h1>Nome do produto</h1>
+              <ProductPrice price={2345.67} />
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição do produto</h2>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis,
+                numquam eum blanditiis rem cumque suscipit. Delectus, sunt
+                explicabo beatae saepe expedita impedit, minima doloribus
+                repellendus repellat quisquam ex atque voluptates?
+              </p>
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetails;
