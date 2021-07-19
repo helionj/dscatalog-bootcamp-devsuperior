b94ee4dd3d75317788b0839cd6e9e8aed15c76f8
helionporto <helionporto@hotmail.com>
2021-07-19 17:22:20 -0400

    Login request

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import ButtonIcon from '../../../../components/ButtonIcon';
 import { useForm } from 'react-hook-form';
 import './styles.css';
+import { requestBackendLogin } from '../../../../util/requests';
 
 type FormData = {
     username: string,
@@ -12,7 +13,13 @@ const Login = () => {
   const { register, handleSubmit } = useForm<FormData>();
 
   const onSubmit = (formData:FormData) => {
-    console.log(formData);
+    requestBackendLogin(formData)
+        .then(response => {
+            console.log("SUCESSO", response.data);
+        })
+        .catch(error => {
+            console.log("ERROR ", error)
+        });
   };
 
   return (
