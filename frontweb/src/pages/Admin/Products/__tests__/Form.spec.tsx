c95d12fb7e517ab3efe8c796c56dd6cbf190ac17
helionporto <helionporto@hotmail.com>
2021-08-22 16:59:17 -0400

    Update Should show toast and redirected when submit form correctly

--- frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -3,7 +3,7 @@ import { Router, useParams } from 'react-router-dom';
 import Form from '../Form';
 import history from '../../../../util/history';
 import userEvent from '@testing-library/user-event';
-import { server } from './fixtures';
+import { productResponse, server } from './fixtures';
 import selectEvent from 'react-select-event';
 import { ToastContainer } from 'react-toastify';
 
@@ -119,5 +119,53 @@ describe('Product create tests', () => {
       const messages = screen.queryAllByText('Campo obrigatório');
       expect(messages).toHaveLength(0);
     });
+  });
+});
+
+describe('Product update tests', () => {
+  beforeEach(() => {
+    (useParams as jest.Mock).mockReturnValue({
+      productId: '2',
+    });
+  });
+
+  test('should render Form', async () => {
+    const text = 'Fazer login';
+
+    render(
+      <Router history={history}>
+        <ToastContainer />
+        <Form />
+      </Router>
+    );
+
+    const nameInput = screen.getByTestId('name');
+    const priceInput = screen.getByTestId('price');
+    const imgUrlInput = screen.getByTestId('imgUrl');
+    const descriptionInput = screen.getByTestId('description');
+    
+    const formElement = screen.getByTestId('form');
+
+
+    await waitFor(() => {
+      expect(nameInput).toHaveValue(productResponse.name);
+      expect(priceInput).toHaveValue(String(productResponse.price));
+      expect(imgUrlInput).toHaveValue(productResponse.imgUrl);
+      expect(descriptionInput).toHaveValue(productResponse.description);
+
+      const ids = productResponse.categories.map(category => String(category.id));
+      expect(formElement).toHaveFormValues({categories: ids});
+    });
+
+    const submitButton = screen.getByRole('button', { name: /salvar/i });
+
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const toastElement = screen.getByText('Produto cadastrado com sucesso');
+
+      expect(toastElement).toBeInTheDocument();
+    });
+    expect(history.location.pathname).toEqual('/admin/products');
   });
 });
